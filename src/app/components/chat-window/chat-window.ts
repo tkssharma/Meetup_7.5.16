06ae97ad342ca5c6345429ae420623bd190bb9a5
Nadeen Southgate <sounad01@CSGICORP.COM>
2016-04-29 09:57:02 +0200

    Updated code

--- src/app/components/chat-window/chat-window.ts
+++ src/app/components/chat-window/chat-window.ts
@@ -1,17 +1,36 @@
-import {Component, Inject} from 'angular2/core';
+import {Component} from 'angular2/core';
+import {FirebaseAuth} from 'angularfire2';
+import {ChatAuth} from '../chat-auth/chat-auth';
 import {ChatList} from '../chat-list/chat-list';
 import {ChatInput} from '../chat-input/chat-input';
-import {FirebaseAuth, AuthProviders} from 'angularfire2';
-import {ChatAuth} from '../chat-auth/chat-auth';
 
 @Component({
-    templateUrl: 'app/components/chat-window/chat-window.html',
-    providers: [AuthProviders],
-    directives: [ChatList, ChatInput, ChatAuth],
-    pipes: []
+    template: `
+    <div class="chat_window">
+        <div class="top_menu">
+            <div class="buttons">
+                <chat-auth></chat-auth>
+            </div>
+            <div class="title">Simple Chat App</div>
+        </div>
+        <chat-list></chat-list>
+        <chat-input></chat-input>
+    </div>
+    `,
+    directives: [ChatList, ChatInput, ChatAuth]
 })
 export class ChatWindow {   
-    constructor(private auth: FirebaseAuth) {
-    }
-    ngOnInit() {}
+    constructor(private auth: FirebaseAuth) {}
 }
+
+
+
+
+
+
+
+
+
+
+
+

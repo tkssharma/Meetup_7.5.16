31b2430a349f88981d1941f1a23dd4bdd8fe7397
Nadeen Southgate <sounad01@CSGICORP.COM>
2016-05-06 08:48:35 +0200

    Added image

--- src/app/components/chat-window/chat-window.ts
+++ src/app/components/chat-window/chat-window.ts
@@ -8,8 +8,9 @@ import {ChatInput} from '../chat-input/chat-input';
     template: `
     <div class="chat_window">
         <chat-topmenu></chat-topmenu>
-        <chat-list></chat-list>
-        <chat-input></chat-input>
+        <chat-list *ngIf="auth | async"></chat-list>
+        <img *ngIf="!(auth | async)" src="app/assets/before.png" class="img-responsive" alt="Responsive image">
+        <chat-input *ngIf="auth | async"></chat-input>
     </div>
     `,
     directives: [ChatList, ChatInput, ChatTopMenu]
